adb483bab026096a9f0df01ca29edbc26f9155aa
kauã gomes <kgs26121967@gmail.com>
2024-07-07 13:18:24 -0300

    adicionando novo projeto

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ AOS.init();
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
